719af24113ec80c46c338769a5a8912917f9d531
christian <vagetto707@aol.com>
2022-03-19 21:16:39 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    const basket = []
+    for (let i = 0; i < cards.length; i++) {
+        basket.push(
+            `Thank you, ${cards[i]}, for the wonderful surprise gift!`
+        )
+            
+    }
+    return basket
+}
+
+
+function countDown(numbers) {
+    let i = numbers;
+    while(i >= 0) { 
+        console.log(i--);
+    
+    }
+    
+}
+    
+       
